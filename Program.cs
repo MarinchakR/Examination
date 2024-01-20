1b0b34f9d12b2a76b3b2c2d61c7dccf08e69ed0e
HugeBee <HugeBee@example.com>
2024-01-20 03:34:09 +0300

    3point

--- Program.cs
+++ Program.cs
@@ -6,3 +6,57 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Write("Введите количество строк в массиве: ");
+string[] stringArray = new string[Convert.ToInt32(Console.ReadLine())];
+Console.WriteLine();
+FillArray(stringArray);
+Console.WriteLine();
+PrintArray(StrLengthThree(stringArray));
+
+string[] FillArray(string[] arrFill)
+{
+    for (int i = 0; i < arrFill.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} строку массива: ");
+        arrFill[i] = Console.ReadLine();
+    }
+    return arrFill;
+}
+
+void PrintArray(string[] arrPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrPrint.Length; i++)
+    {
+        Console.Write($"\"{arrPrint[i]}\"");
+        if (i != arrPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+string[] StrLengthThree(string[] arrCheck)
+{
+    int count = 0;
+    for (int i = 0; i < arrCheck.Length; i++)
+    {
+        if (arrCheck[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] array = new string[count];
+    int index = 0;
+    for (int i = 0; i < arrCheck.Length; i++)
+    {
+        if (arrCheck[i].Length <= 3)
+        {
+            array[index] = arrCheck[i];
+            index++;
+        }
+    }
+    return array;
+}
